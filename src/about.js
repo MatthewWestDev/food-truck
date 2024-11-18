1ff5b31204ec8af38d50ae740888ad222a9716fd
Matthew West <128643572+MatthewWestDev@users.noreply.github.com>
2024-11-18 12:47:46 -0600

    Completes basic site setup

---
+++ src/about.js
@@ -0,0 +1,3 @@
+{/* <img class="full-width-img" src="images/ss-one.png">
+<h2>Lorem Ipsum About Us!</h2>
+<p>About us lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+// header elements
+const h1 = document.querySelector("h1");
+import starImg from "./images/star-outline.svg";
+const logo = document.createElement("img");
+logo.src = starImg;
+h1.appendChild(logo);
+
